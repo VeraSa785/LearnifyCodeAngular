7c31602194a7c6c2198b1994648321e5f82bf783
Vera <vera@contorra.com>
2024-07-25 15:29:56 -0700

    Added angular material to project dependencies, and tested/added default buttons and inputs angular material implementation.

--- frontend-angular/src/app/welcome-page/welcome-page.component.ts
+++ frontend-angular/src/app/welcome-page/welcome-page.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {MatDivider} from "@angular/material/divider";
+import {MatChip, MatChipSet} from "@angular/material/chips";
+import {
+  MatAnchor,
+  MatButton,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconButton,
+  MatMiniFabButton
+} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatDivider, MatChipSet, MatChip, MatButton, MatAnchor, MatIcon, MatMiniFabButton, MatFabButton, MatIconButton, MatFormField, MatLabel, MatInput, MatFabAnchor],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.css'
 })
--- frontend-angular/src/main.ts
+++ frontend-angular/src/main.ts
@@ -1,15 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
 import routes from "./app/app.routes";
 import {provideRouter} from "@angular/router";
-
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
+import {provideAnimations} from "@angular/platform-browser/animations";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideAnimations(),
+    provideRouter(routes), provideAnimationsAsync()
   ]
 })
   .catch(err => console.error(err));
